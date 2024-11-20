0b2a9048238b4719259512a4824da4550a540906
shawal-mbalire <mbalireshawal@gmail.com>
2024-11-20 12:55:40 +0300

    made_tree_better

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,6 +1,8 @@
-
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- nvim tree keybindings
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
+vim.keymap.set("n", "<leader>tt", vim.cmd.NvimTreeToggle)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -12,12 +14,12 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
+-- vim.keymap.set("n", "<leader>vwm", function()
+--     require("vim-with-me").StartVimWithMe()
+-- end)
+-- vim.keymap.set("n", "<leader>svwm", function()
+--     require("vim-with-me").StopVimWithMe()
+-- end)
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
@@ -29,7 +31,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
 
 -- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -62,8 +64,8 @@ vim.keymap.set(
 )
 
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/packer/init.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+-- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/packer/init.lua<CR>");
+-- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -3,36 +3,36 @@ return {
     version = "*",
     lazy = false,
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
+        "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      -- disable netrw at the very start of your init.lua
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
+        -- disable netrw at the very start of your init.lua
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
 
-      -- settings
-      local nvim_tree = require("nvim-tree")
-      nvim_tree.setup({
-        sort = {
-            sorter = "case_sensitive",
-        },
-        view = {
-            width = 30,
-            side = "right",
-        },
-        renderer = {
-            group_empty = true,
-        },
-        filters = {
-            dotfiles = false,
-            git_ignored = false,
-        },
-      })
+        -- settings
+        local nvim_tree = require("nvim-tree")
+        nvim_tree.setup({
+            sort = {
+                sorter = "case_sensitive",
+            },
+            view = {
+                width = 30,
+                side = "right",
+            },
+            renderer = {
+                group_empty = true,
+            },
+            filters = {
+                dotfiles = false,
+                git_ignored = false,
+            },
+        })
 
-      local nvim_tree_api = require("nvim-tree.api")
-      --  key bindings
-      vim.keymap.set("n", "<leader>t", function() nvim_tree_api.tree.toggle() end)
-      -- vim.keymap.set("n", "<leader>t", function() nvim_tree_api.tree.close_in_this_tab() end)
-      -- vim.keymap.set("n", "<leader>y", function() nvim_tree_api.tree.open() end)
+        local nvim_tree_api = require("nvim-tree.api")
+        --  key bindings
+        -- vim.keymap.set("n", "<leader>t", function() nvim_tree_api.tree.toggle() end)
+        -- vim.keymap.set("n", "<leader>t", function() nvim_tree_api.tree.close_in_this_tab() end)
+        vim.keymap.set("n", "<leader>t", function() nvim_tree_api.tree.open() end)
     end,
-  }
+}
