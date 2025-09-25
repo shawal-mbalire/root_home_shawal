a6c5c93b81ac2af7b175200ae5c89da8f3ee337f
shawal-mbalire <mbalireshawal@gmail.com>
2025-09-25 14:53:51 +0300

    Update configuration and themes

--- .config/nvim/lua/custom/init.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.colorcolumn = "80"
-
--- Set tabline highlight groups to semi-transparent with a subtle background
-vim.api.nvim_set_hl(0, "TabLine", { bg = "#1e1e1e", blend = 80 })
-vim.api.nvim_set_hl(0, "TabLineSel", { bg = "#282828", blend = 80 })
-vim.api.nvim_set_hl(0, "TabLineFill", { bg = "#1e1e1e", blend = 80 })
