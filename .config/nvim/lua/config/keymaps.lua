3eb5cacada72def6a1063884a7bfffeed5f49cb6
shawal-mbalire <mbalireshawal@gmail.com>
2025-11-26 16:48:36 +0300

    feat: fixed nvim explore
    - explorer now opens in cwd same as snacks.picker

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,5 +4,13 @@
 
 -- Override to open in cwd
 vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files({ cwd = vim.fn.getcwd() }) end, { desc = "Find Files (cwd)" })
+vim.keymap.set("n", "<leader>sg", function() Snacks.picker.grep({ cwd = vim.fn.getcwd() }) end, { desc = "Grep (cwd)" })
+vim.keymap.set("n", "<leader>sb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
 vim.keymap.set("n", "<leader>ft", function() Snacks.terminal(nil, { cwd = vim.fn.getcwd() }) end, { desc = "Terminal (cwd)" })
 vim.keymap.set("n", "<leader>e", function() Snacks.explorer({ cwd = vim.fn.getcwd() }) end, { desc = "Explorer Snacks (cwd)" })
+
+-- Tmux integration
+vim.keymap.set("n", "<leader>tn", "<cmd>!tmux new-window<cr>", { desc = "New Tmux Window" })
+vim.keymap.set("n", "<leader>ts", "<cmd>!tmux split-window<cr>", { desc = "Split Tmux Window" })
+vim.keymap.set("n", "<leader>tv", "<cmd>!tmux split-window -h<cr>", { desc = "Vertical Split Tmux" })
