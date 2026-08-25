a1b7b56d244d4e3d4a1dba641f1a52cc419ef809
shawal-mbalire <mbalireshawal@gmail.com>
2026-08-25 23:28:27 +0300

    config: nushell
    - time to retry nushell

--- dot-config/fish/functions/fish_prompt.fish
+++ dot-config/fish/functions/fish_prompt.fish
@@ -3,10 +3,12 @@ function fish_prompt
                 echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
         end
     
-    set_color -o
+        echo -n (set_color blue)(prompt_pwd)' '
+    
+        set_color -o
         if fish_is_root_user
                 echo -n (set_color red)'# '
         end
         echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
-        set_color normal
+        set_color --reset
 end
