afffaf32386ad51c18a4a9da4a4fbba8bfaf7332
shawal-mbalire <mbalireshawal@gmail.com>
2026-09-02 15:08:49 +0300

    feat: add tuicr config and nvim auto-reload
    - tuicr: catppuccin-mocha theme, unified diff, conventional comments
    - nvim: auto-reload files changed externally (FocusGained, BufEnter, CursorHold)

--- dot-config/nvim/lua/config/autocmds.lua
+++ dot-config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-reload files when changed externally (e.g. tuicr editing)
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  callback = function()
+    vim.defer_fn(function()
+      vim.cmd("checktime")
+    end, 50)
+  end,
+})
--- dot-config/nvim/lua/config/options.lua
+++ dot-config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.autoread = true
+vim.opt.updatetime = 200
+
 -- Workaround for Neovim 0.12.5 bug: vim.fs.abspath asserts uv.cwd() ~= nil
 -- but uv.cwd() can return nil during BufNewFile autocommands
 local uv = vim.uv or vim.loop
